d43aba09183be5a196f9695c52515c5920a8ac0e
Mario Finelli <mario@finel.li>
2020-11-20 20:55:05 -0500

    Remove useless hosts file

--- bootstrap.bash
+++ bootstrap.bash
@@ -24,7 +24,7 @@ fi
 # yubikey needs this ahead of time to work
 gpg --import 4DA7BCBA.asc
 
-ansible-playbook --inventory hosts --vault-id ${VAULT_ID} \
+ansible-playbook --vault-id ${VAULT_ID} \
   --extra-vars whoami="$(whoami)" \
   --extra-vars whoami_group="$(id -gn)" \
   --extra-vars mtype=$mtype \
