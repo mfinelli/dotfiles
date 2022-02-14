3b54412466ccaa06181ef0e570b39cac36cab61b
Mario Finelli <mario@finel.li>
2022-02-14 23:24:12 +0100

    Add ubuntu config

--- roles/tmux/files/os.bash
+++ roles/tmux/files/os.bash
@@ -20,6 +20,8 @@ elif [[ $(uname) == Linux ]]; then
       echo -e '\uF303' > "${HOME}/.tmux/os.conf"
     elif [[ $distro == "Debian GNU/Linux" ]]; then
       echo -e '\uF306' > "${HOME}/.tmux/os.conf"
+    elif [[ $distro == "Ubuntu" ]]; then
+      echo -e '\uF31b' > "${HOME}/.tmux/os.conf"
     elif [[ $distro == "CentOS Linux" ]]; then
       echo -e '\uF304' > "${HOME}/.tmux/os.conf"
     else
