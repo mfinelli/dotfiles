b378b04cbcbde35e4770b2cfd1e38e06a4465460
Mario Finelli <mario@finel.li>
2025-05-09 12:32:39 +0200

    Move tmux configuration to .config

--- roles/tmux/files/os.bash
+++ roles/tmux/files/os.bash
@@ -8,30 +8,32 @@ if [[ $# -ne 0 ]]; then
   exit 1
 fi
 
+osconffile="${HOME}/.config/tmux/os.conf"
+
 # find "nerdfont-complete" and grab the icons from the list
 # https://github.com/romkatv/powerlevel10k/blob/master/internal/icons.zsh
 if [[ $(uname) == Darwin ]]; then
-  echo -e '\uF179' > "${HOME}/.tmux/os.conf"
+  echo -e '\uF179' > "$osconffile"
 elif [[ $(uname) == Linux ]]; then
   if [[ -f /etc/os-release ]]; then
     distro="$(grep ^NAME /etc/os-release | awk -F\" '{print $2}')"
 
     if [[ $distro == "Arch Linux" ]]; then
-      echo -e '\uF303' > "${HOME}/.tmux/os.conf"
+      echo -e '\uF303' > "$osconffile"
     elif [[ $distro == "Debian GNU/Linux" ]]; then
-      echo -e '\uF306' > "${HOME}/.tmux/os.conf"
+      echo -e '\uF306' > "$osconffile"
     elif [[ $distro == "Ubuntu" ]]; then
-      echo -e '\uF31b' > "${HOME}/.tmux/os.conf"
+      echo -e '\uF31b' > "$osconffile"
     elif [[ $distro == "CentOS Linux" ]]; then
-      echo -e '\uF304' > "${HOME}/.tmux/os.conf"
+      echo -e '\uF304' > "$osconffile"
     else
-      echo -e '\uF17C' > "${HOME}/.tmux/os.conf"
+      echo -e '\uF17C' > "$osconffile"
     fi
   else
-    echo -e '\uF17C' > "${HOME}/.tmux/os.conf"
+    echo -e '\uF17C' > "$osconffile"
   fi
 else
-  echo -e '☺' > "${HOME}/.tmux/os.conf"
+  echo -e '☺' > "$osconffile"
 fi
 
 exit 0
